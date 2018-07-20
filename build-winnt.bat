275858007d89ac5a776eaead18238ef79a6526c9
Paul Austin <paul.austin@revolsys.com>
2018-07-20 11:29:44 -0700

    link libs

--- build-winnt.bat
+++ build-winnt.bat
@@ -2,8 +2,7 @@
 set VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
 
 set PATH=%VSINSTALLDIR%\Common7\IDE;%PATH%
-set PATH=%VSINSTALLDIR%\VC\bin;%PATH%
-set PATH=%VSINSTALLDIR%\VC\bin\x86_amd64;%PATH%
+set PATH=%VSINSTALLDIR%\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64;%PATH%
 
 if not exist "target\classes\natives\windows_64\" (
   md target\classes\natives\windows_64
