a91efeb2399d055e6d2fe501cdb927daf5d27675
Paul Austin <paul.austin@revolsys.com>
2018-07-20 10:34:38 -0700

    md

--- build-winnt.bat
+++ build-winnt.bat
@@ -4,4 +4,9 @@ set VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 14.0
 set PATH=%VSINSTALLDIR%\Common7\IDE;%PATH%
 set PATH=%VSINSTALLDIR%\VC\bin;%PATH%
 set PATH=%VSINSTALLDIR%\VC\bin\x86_amd64;%PATH%
+
+if not exist "target\classes\natives\windows_64\" (
+  md target\classes\natives\windows_64
+)
+
 nmake ARCH=x86_64 /f Makefile.nmake
